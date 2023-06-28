afd4df9225a35f0f012dbacf0aa97aab1b4f05e9
icaro <tony@axiomeconcept.com>
2023-06-28 14:51:06 +0200

    first commit

---
+++ REAME.md
@@ -0,0 +1,2 @@
+Petit simulateur d'api rest en langage go, 
+analyse le header de la requête "application/json" et la "APIKey"
---
+++ apirest_server.go
@@ -0,0 +1,105 @@
+/*
+Go server apirest simulation  A.Villanueva
+curl http://localhost:8080/api/v1/gateways
+curl -X GET -H "Content-Type: application/json" -H "APIKey: cd66a4f9-8a9b-4a8c-a02a-ff2d7d1c3e3c" <URL>
+curl -X GET -H "Content-Type: application/json" -H "APIKey: ef3a5fb2-8d89-4f07-ae6c-9b2692ef9f5f" <URL>
+
+curl -X GET -H "Content-Type: application/json" -H "APIKey: cd66a4f9-8a9b-4a8c-a02a-ff2d7d1c3e3c" 127.0.0.1:8080/api/v1/gateways
+*/
+
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	URL_BASE = "/api/v1"
+)
+
+type ErrorResponse struct {
+	Error string `json:"error"`
+}
+
+type Data struct {
+	TotalCount int       `json:"totalCount"`
+	Results    []Gateway `json:"results"`
+}
+
+type Gateway struct {
+	GatewayID          int    `json:"gatewayId"`
+	GatewayLabel       string `json:"gatewayLabel"`
+	CMac               string `json:"cMac"`
+	LorawanMac         string `json:"lorawanMac"`
+	LorawanConnected   bool   `json:"lorawanConnected"`
+	LorawanLastPing    string `json:"lorawanLastPing"`
+	ClovernetConnected bool   `json:"clovernetConnected"`
+	ClovernetLastPing  string `json:"clovernetLastPing"`
+	LevelID            int    `json:"levelId"`
+}
+
+var gateways = Data{
+	TotalCount: 2,
+	Results: []Gateway{
+		{
+			GatewayID:          1,
+			GatewayLabel:       "00800000A0008796_FBR",
+			CMac:               "55:66:77:88",
+			LorawanMac:         "01:02:03:04:05:06",
+			LorawanConnected:   true,
+			LorawanLastPing:    "2023-06-28T10:00:00Z",
+			ClovernetConnected: false,
+			ClovernetLastPing:  "",
+			LevelID:            3,
+		},
+		{
+			GatewayID:          2,
+			GatewayLabel:       "00800000A00093B7-AXIOME",
+			CMac:               "11:22:33:44",
+			LorawanMac:         "AA:BB:CC:DD:EE:FF",
+			LorawanConnected:   false,
+			LorawanLastPing:    "",
+			ClovernetConnected: true,
+			ClovernetLastPing:  "2023-06-28T12:30:00Z",
+			LevelID:            2,
+		},
+	},
+}
+
+// getGateways
+func getGateways(c *gin.Context) {
+	if testHeader(c) {
+		c.IndentedJSON(http.StatusOK, gateways)
+	}
+	// Erreur json message
+	c.AbortWithStatusJSON(http.StatusInternalServerError, ErrorResponse{"Internal Server Error"})
+}
+
+func testHeader(c *gin.Context) bool {
+	//Vérifier la clé API
+	apiKey := c.GetHeader("APIKey")
+	if apiKey != "c6aa27d6-fc9b-4223-8665-1de556cd7a09" && apiKey != "b4e77488-5330-2a27-2d07-4ce64d0af88d" {
+		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
+		return false
+	}
+
+	//Vérifiez le header Content-Type
+	contentType := c.GetHeader("Content-Type")
+	if contentType != "application/json" {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "Invalid Content-Type"})
+		return false
+	}
+
+	return true
+}
+
+func main() {
+	router := gin.Default()
+	router.GET(URL_BASE+"/gateways", getGateways)
+
+	router.Run("localhost:8080")
+	os.Exit(0)
+}
